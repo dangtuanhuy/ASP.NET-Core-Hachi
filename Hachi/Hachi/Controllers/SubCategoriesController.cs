caf84cd8df880f930857945817cb9a770f1ab820
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-12-18 16:35:05 +0700

    Core

--- Hachi/Hachi/Controllers/SubCategoriesController.cs
+++ Hachi/Hachi/Controllers/SubCategoriesController.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Hachi.Models;
 using Hachi.Data;
-
+using Microsoft.EntityFrameworkCore;
+using Hachi.Models.SubCategoryViewModels;
 namespace Hachi.Controllers
 {
     public class SubCategoriesController : Controller
@@ -18,8 +19,19 @@ namespace Hachi.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var subcategory = from p in _context.SubCategory join d in _context.Category on p.CategoryId equals d.Id select 
-            return View();
+            var subcategory = _context.SubCategory.Include(p=>p.Category);
+            return View(await subcategory.ToListAsync());
+        }
+        //GET Action for Create
+        public IActionResult Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = _context.Category.ToList(),
+                SubCategory = new SubCategory(),
+                SubCategoryList = _context.SubCategory.OrderBy(p => p.SubCategoryName).Select(p => p.SubCategoryName).ToList()
+            };
+            return View(model);
         }
     }
 }
