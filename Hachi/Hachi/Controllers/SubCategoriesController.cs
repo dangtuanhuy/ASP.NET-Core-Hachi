9d6d200b70ba1c3b0ba56112ba0208a2dab31e03
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-12-19 14:19:43 +0700

    SubCategory

--- Hachi/Hachi/Controllers/SubCategoriesController.cs
+++ Hachi/Hachi/Controllers/SubCategoriesController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Hachi.Models;
-using Hachi.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Hachi.Models.SubCategoryViewModels;
+using Hachi.Data;
+using Hachi.Models;
+
 namespace Hachi.Controllers
 {
     public class SubCategoriesController : Controller
@@ -17,21 +18,143 @@ namespace Hachi.Controllers
         {
             _context = context;
         }
+
+        // GET: SubCategories
         public async Task<IActionResult> Index()
         {
-            var subcategory = _context.SubCategory.Include(p=>p.Category);
-            return View(await subcategory.ToListAsync());
+            var applicationDbContext = _context.SubCategory.Include(s => s.Category);
+            return View(await applicationDbContext.ToListAsync());
         }
-        //GET Action for Create
+
+        // GET: SubCategories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await _context.SubCategory
+                .Include(s => s.Category)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Create
         public IActionResult Create()
         {
-            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "CategoryName");
+            return View();
+        }
+
+        // POST: SubCategories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,SubCategoryName,CategoryId")] SubCategory subCategory)
+        {
+            if (ModelState.IsValid)
             {
-                CategoryList = _context.Category.ToList(),
-                SubCategory = new SubCategory(),
-                SubCategoryList = _context.SubCategory.OrderBy(p => p.SubCategoryName).Select(p => p.SubCategoryName).ToList()
-            };
-            return View(model);
+                _context.Add(subCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "CategoryName", subCategory.CategoryId);
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await _context.SubCategory.SingleOrDefaultAsync(m => m.Id == id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "CategoryName", subCategory.CategoryId);
+            return View(subCategory);
+        }
+
+        // POST: SubCategories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,SubCategoryName,CategoryId")] SubCategory subCategory)
+        {
+            if (id != subCategory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(subCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SubCategoryExists(subCategory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "CategoryName", subCategory.CategoryId);
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await _context.SubCategory
+                .Include(s => s.Category)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(subCategory);
+        }
+
+        // POST: SubCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var subCategory = await _context.SubCategory.SingleOrDefaultAsync(m => m.Id == id);
+            _context.SubCategory.Remove(subCategory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SubCategoryExists(int id)
+        {
+            return _context.SubCategory.Any(e => e.Id == id);
         }
     }
 }
